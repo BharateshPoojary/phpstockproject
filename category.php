19b8cefc0b02cf345886b82722c1bc93b19c4c4c
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-11-30 15:09:03 +0530

    dbConnected

--- category.php
+++ category.php
@@ -1,3 +1,11 @@
+<?php include "db_Connect.php"
+
+$select_category="SELECT * FROM mstcategory WHERE isVisible='Y'";
+$select_category_result = sqlsrv_query($conn,$select_category);
+
+
+
+?>
 <!doctype html>
 <html lang="en">
 
@@ -86,6 +94,109 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <?php foreach ($response["Cat"] as $Cat): ?>
+                    <div class="col-md-6 col-lg-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex align-items-start">
+                                    <div class="bg-warning-subtle text-warning d-inline-block px-4 py-3 rounded  viewSub"
+                                        data-cat-id="<?= $Cat['catId'] ?>">
+                                        <!-- <i class="ti ti-brand-google-drive display-6"></i> -->
+                                        <img src="<?= $Cat['catImg'] ?>" class="rounded img-fluid">
+                                    </div>
+                                    <div class="ms-auto">
+                                        <div class="dropdown dropstart">
+                                            <a href="javascript:void(0)" class="link text-dark"
+                                                id="dropdownMenuButton" data-bs-toggle="dropdown"
+                                                aria-expanded="false">
+                                                <i class="ti ti-dots fs-7"></i>
+                                            </a>
+                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="">
+                                                <li>
+                                                    <a class="dropdown-item" href="javascript:void(0)"
+                                                        data-bs-toggle="modal"
+                                                        data-bs-target="#view<?= $Cat['catId'] ?>">Edit</a>
+                                                </li>
+                                                <li>
+                                                    <a class="dropdown-item sa-confirm" href="javascript:void(0)"
+                                                        data-id="<?= $Cat['catId'] ?>"
+                                                        data-catname="<?= $Cat['catName'] ?>">Delete</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt-5">
+                                    <h4 class="card-title"><?= $Cat['catName'] ?></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="view<?= $Cat['catId'] ?>" class="modal fade" tabindex="-1" aria-modal="true"
+                        role="dialog">
+                        <div class="modal-dialog modal-dialog-scrollable modal-lg">
+                            <div class="modal-content">
+                                <div class="modal-body">
+                                    <div class="text-center mt-2 mb-4">
+                                        Edit Category
+                                    </div>
+                                    <form action="CURD_category.php" method="post" enctype="multipart/form-data"
+                                        class="ps-3 pr-3">
+                                        <input type="text" name="action" value="update" hidden>
+                                        <input type="text" name="catId" value="<?= $Cat['catId'] ?>" hidden>
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div class="mb-3">
+                                                    <label for="inputcom" class="form-label">Name</label>
+                                                    <input type="text" class="form-control" id="inputcom"
+                                                        placeholder="Category Here" name="catName"
+                                                        value="<?= $Cat['catName'] ?>">
+                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Select File</label>
+                                                    <div class="input-group flex-nowrap">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text">Upload</span>
+                                                        </div>
+                                                        <div class="custom-file">
+                                                            <input type="file" class="form-control"
+                                                                id="inputGroupFile01" value="<?= $Cat['catImg'] ?>"
+                                                                accept=".png, .jpg,.jpeg,image/*" name="catImg"
+                                                                onchange="img_pathUrl(this, 'img_url1<?= $Cat['catId'] ?>');">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-6 col-xxl-4">
+                                                <div class="mb-3">
+                                                    <div class="mb-3 d-flex justify-content-center">
+                                                        <?php if (!empty($Cat['catImg'])) { ?>
+                                                            <img src="<?= $Cat['catImg'] ?>"
+                                                                style="height: 150px; width: 3.5cm;" alt="..."
+                                                                id="img_url1<?= $Cat['catId'] ?>">
+                                                        <?php } else { ?>
+                                                            <img style="height: 150px; width: 3.5cm; display:none"
+                                                                id="img_url1<?= $Cat['catId'] ?>" />
+                                                        <?php } ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="mb-3 text-center">
+                                            <button class="btn btn-rounded bg-info-subtle text-info" type="submit">
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
       </div>
     </div>
@@ -107,6 +218,7 @@
             imgUrl.style.display="none";
         }
     }
+    const getResponse = ""
 
   </script>
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script><!-- using axios -->
---
+++ db_Connect.php
@@ -0,0 +1,20 @@
+<?php
+date_default_timezone_set('Asia/Calcutta');
+$serverName = "103.14.97.85";
+$connectionInfo = array(
+    "Database" => "stockdb",
+    "UID" => "stock",
+    "PWD" => "_Yz33Q4c6hjTyxsvg",
+    "CharacterSet" => "UTF-8"
+);
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+$AdminURL = 'http://stock.swiftmore.in/Admin/';
+$baseURL = 'http://stock.swiftmore.in/';
+
+if ($conn) {
+    // echo "Connection established";
+} else {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
